9d835cdabefaf4e72f92b32704f91e76ecc7c0a3
Molina <elison_li@hotmail.com>
2024-02-10 17:21:15 -0300

    Obtendo valores da API

--- CriptoVisionFront/src/app/app.component.ts
+++ CriptoVisionFront/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Moeda {
+  nome: string;
+  ultimaHora: number
+  vinteQuatroHoras: number
+  seteDias: number
+  trintaDias:number
+  precoAtual: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'CriptoVisionFront';
+export class AppComponent implements OnInit {
+  listaMoedas: Moeda[] = [];
+  titulos: string[] = [
+    '#',
+    'Moeda',
+    'Preço Atual',
+    'Preço 1h',
+    'Preço 24h',
+    'Preço 7d',
+    'Preço 30d'
+  ]
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Moeda[]>(
+        'http://localhost:8080/consulta-cripto/listaCriptosP?chaveApi=password'
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.listaMoedas = res
+        },
+        (err) => console.log(err)
+      );
+  }
 }
